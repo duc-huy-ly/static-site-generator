7ed0c08659370fc566aac0157efc22b5c47c9221
Duc Huy LY <78007468+duc-huy-ly@users.noreply.github.com>
2026-01-01 04:04:53 +0100

    text node

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    node = TextNode("Mads Mikkelson", TextType.PLAIN, "https://en.wikipedia.org/wiki/Mads_Mikkelsen")
+    print(node)
+if __name__ == '__main__':
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINKS = "links"
+    IMAGES = "images"
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.__text = text
+        self.__text_type = text_type
+        self.__url = url
+    
+    def __eq__(self, other):
+        return self.__text == other.__text and self.__text_type == other.__text_type and self.__url == other.__url
+    
+    def __repr__(self):
+        return f"TextNode({self.__text}, {self.__text_type.value}, {self.__url})" 
