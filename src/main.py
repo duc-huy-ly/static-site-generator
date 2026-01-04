0031ad3b532b9cf8d14e6b521290f116442fc0a4
Duc Huy LY <78007468+duc-huy-ly@users.noreply.github.com>
2026-01-04 13:43:56 +0100

    recursive copy of static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
 from textnode import *
+import os
+import shutil
 
 def main():
-    node = TextNode("Mads Mikkelson", TextType.PLAIN, "https://en.wikipedia.org/wiki/Mads_Mikkelsen")
-    print(node)
+    copy_files("static/", "public/")
+    return 0
+
     
+def copy_files(src, dst, is_root=True):
+    if os.path.exists(dst) and is_root:
+        shutil.rmtree(dst)
+    if is_root:
+        os.mkdir(dst)
+    contents = os.listdir(src)
+    print(contents)
+    for content in contents:
+        src_path = os.path.join(src, content)
+        dst_path = os.path.join(dst, content)
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+        elif os.path.isdir(src_path):
+            os.mkdir(dst_path)
+            copy_files(src_path, dst_path, is_root=False)
+            
 if __name__ == '__main__':
     main()
+    
