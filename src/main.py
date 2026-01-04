d76b86252106d1e181459a0ebf795895c0f0e47d
Duc Huy LY <78007468+duc-huy-ly@users.noreply.github.com>
2026-01-04 21:47:04 +0100

    generat pages recursively

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from md_to_blocks import markdown_to_html_node
 
 def main():
     copy_files("static/", "public/")
-    generate_page("content/index.md", "template.html", "public")
+    generate_page_recursive("content/", "template.html", "public")
     return 0
     
 def copy_files(src, dst, is_root=True):
@@ -46,7 +46,40 @@ def generate_page(src_path, template_path, dst_path):
     file_name = src_path.split('/')[-1].split('.')[0]
     with open(os.path.join(dst_path, f"{file_name}.html"), "x") as f:
         f.write(endfile)
+        
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    # Make sure they exist
+    check_path([dir_path_content, template_path])
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
+        
+    directories = os.listdir(dir_path_content)
+    for directory in directories:
+        # if it's a markdown file
+        content_path = os.path.join(dir_path_content, directory)
+        dest_dir_content_path = os.path.join(dest_dir_path, directory)
+        if os.path.isfile(content_path) and directory.endswith('.md'):
+            # we convert to html and create the file in public
+            fd =  open(content_path) 
+            file_content = fd.read()
+            file_html = markdown_to_html_node(file_content).to_html()
+            filename = directory.split('.md')[0] + ".html"
+            file_title = extract_title(file_content)
+            # Write the content in template
+            template_html = open(template_path).read()
+            result_html = template_html.replace("{{ Content }}", file_html).replace("{{ Title }}", file_title)
+            # Create the new html file in the destination directory
+            with open(os.path.join(dest_dir_path, filename),"x") as fd1:
+                fd1.write(result_html)
+        if os.path.isdir(content_path):
+            # recursive call
+            generate_page_recursive(content_path, template_path, dest_dir_content_path)
     
+def check_path(paths):
+    for path in paths:
+        if not os.path.exists(path):
+            raise Exception(path, " doest not exist")
+        
 if __name__ == '__main__':
     main()
     
